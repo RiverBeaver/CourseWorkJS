5e90a3bf9975050bcbb8a9a2016561dd1bfd7796
RiverBeaver <maria.a.badanina@gmail.com>
2024-08-05 13:29:31 +0300

    making edits to the registration section

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-import { auth } from "./scripts/controller/login.js";
-import { registration } from "./scripts/controller/registration.js";
-import { taskCreate } from "./scripts/controller/taskCreate.js";
-import { showMainPage } from "./scripts/controller/mainPage.js";
-import { filterView, view } from "./index.js";
-import { profileFunctional, exit } from "./scripts/controller/profile.js";
+import { auth } from './scripts/controller/login.js';
+import { registration } from './scripts/controller/registration.js';
+import { taskCreate } from './scripts/controller/taskCreate.js';
+import { showMainPage } from './scripts/controller/mainPage.js';
+import { filterView, view } from './index.js';
+import { profileFunctional, exit } from './scripts/controller/profile.js';
 
 // document.querySelectorAll('.taskTable').forEach(table => {
-    //     table.addEventListener('scroll', e => {
+//     table.addEventListener('scroll', e => {
 //         console.dir(e.target.style);
 //     })
 // })
 
 // document.write('<base href="' + document.location + '" />');
-
 
 let body = document.body;
 let hash = location.hash;
@@ -20,18 +19,18 @@ let hash = location.hash;
 // CRUTCH
 
 export function crutchLogin() {
-    history.pushState('', '', '#table');
-    hash = location.hash;
-    body.innerHTML = '';
-    choosePage();
+  history.pushState('', '', '#table');
+  hash = location.hash;
+  body.innerHTML = '';
+  choosePage();
 }
 
 function crutchProfile() {
-    showProfileLayout();
+  showProfileLayout();
 }
 
 function crutchTaskCreation() {
-    showTaskCreation();
+  showTaskCreation();
 }
 
 ////////////////////////////////////////////////////////
@@ -40,140 +39,142 @@ function crutchTaskCreation() {
 
 choosePage();
 
-window.addEventListener("hashchange", e => {
-    body.innerHTML = '';
-    hash = location.hash;
-    choosePage();
-})
+window.addEventListener('hashchange', (e) => {
+  body.innerHTML = '';
+  hash = location.hash;
+  choosePage();
+});
 
 function choosePage() {
-    if (!hash) {
-        history.pushState('', '', '#login')
-        gotoAuthorizationPage();
-        gotoLoginForm();
-    }
-    else if (hash === '#login') {
-        gotoAuthorizationPage();
-        gotoLoginForm();
-    }
-    else if (hash === '#register') {
-        gotoAuthorizationPage();
-        gotoRegistrationForm();
-    }
-    else if (hash === '#table') {
-        gotoMainPage();
-        gotoTableLayout();
-    } else {
-        showError();
-        body.querySelector('.errorOverlay .text').innerText = 'The page does not exist.'
-    }
-};
-
-
-
-
+  if (!hash) {
+    history.pushState('', '', '#register');
+    // gotoAuthorizationPage();
+    // gotoLoginForm();
+    gotoAuthorizationPage();
+    gotoRegistrationForm();
+  } else if (hash === '#login') {
+    gotoAuthorizationPage();
+    gotoLoginForm();
+  } else if (hash === '#register') {
+    gotoAuthorizationPage();
+    gotoRegistrationForm();
+  } else if (hash === '#table') {
+    gotoMainPage();
+    gotoTableLayout();
+  } else {
+    showError();
+    body.querySelector('.errorOverlay .text').innerText =
+      'The page does not exist.';
+  }
+}
 
 function gotoAuthorizationPage() {
-    body.append(view.get('authorizationPage'));
+  body.append(view.get('authorizationPage'));
 }
 
 function gotoLoginForm() {
-    document.querySelector('.functional').append(view.get('loginFunctional'));
-    auth();
+  document.querySelector('.functional').append(view.get('loginFunctional'));
+  auth();
 }
 
 function gotoRegistrationForm() {
-    document.querySelector('.functional').append(view.get('registrationFunctional'));
-    registration();
+  document
+    .querySelector('.functional')
+    .append(view.get('registrationFunctional'));
+  registration();
 }
 
 function gotoMainPage() {
-    body.append(view.get('header'));
-    body.querySelector('main').append(view.get('toolbar'));
+  body.append(view.get('header'));
+  body.querySelector('main').append(view.get('toolbar'));
 }
 
 function gotoTableLayout() {
-    showMainPage();
+  showMainPage();
 
-    ////////////////////////////////////////////// !!!
+  ////////////////////////////////////////////// !!!
 
-    document.querySelector('header .user').addEventListener('click', click => {
-        showProfileOptions();
-        // showProfileLayout();
-    })
+  document.querySelector('header .user').addEventListener('click', (click) => {
+    showProfileOptions();
+    // showProfileLayout();
+  });
 }
 
 function showProfileOptions() {
-    hideProfileOptions();
-    body.append(view.get('profileOptions'));
+  hideProfileOptions();
+  body.append(view.get('profileOptions'));
 
-    body.onclick = function(click) {
-        if (!(click.target.closest('.profileOptions') || click.target.closest('.user'))) {
-            hideProfileOptions();
-            body.onclick = '';
-        } else if (click.target.closest('.profileOptions')) {
-            if (click.target.closest('.profile')) {
-                hideProfileOptions();
-                showProfileLayout();
-            } else if (click.target.closest('.logOut')) {
-                history.pushState('', '', '#login')
-                gotoAuthorizationPage();
-                gotoLoginForm();
-                exit();
-            }
-        }
+  body.onclick = function (click) {
+    if (
+      !(
+        click.target.closest('.profileOptions') || click.target.closest('.user')
+      )
+    ) {
+      hideProfileOptions();
+      body.onclick = '';
+    } else if (click.target.closest('.profileOptions')) {
+      if (click.target.closest('.profile')) {
+        hideProfileOptions();
+        showProfileLayout();
+      } else if (click.target.closest('.logOut')) {
+        history.pushState('', '', '#login');
+        gotoAuthorizationPage();
+        gotoLoginForm();
+        exit();
+      }
     }
+  };
 }
 
 export function hideProfileOptions() {
-    if (document.querySelector('.profileOptions')) body.removeChild(document.querySelector('.profileOptions'));
+  if (document.querySelector('.profileOptions'))
+    body.removeChild(document.querySelector('.profileOptions'));
 }
-
 
 function showProfileLayout() {
-    body.append(view.get('profileLayoutTemplate'));
-    profileFunctional()
-    ////////////////////////////////////////////// !!!
+  body.append(view.get('profileLayoutTemplate'));
+  profileFunctional();
+  ////////////////////////////////////////////// !!!
 
-    document.querySelector('.profileLayout .crossIco').addEventListener('click', click => {
-        hideProfileLayout();
-    })
+  document
+    .querySelector('.profileLayout .crossIco')
+    .addEventListener('click', (click) => {
+      hideProfileLayout();
+    });
 }
 
-
 export function hideProfileLayout() {
-    body.removeChild(document.querySelector('.profileLayout'));
+  body.removeChild(document.querySelector('.profileLayout'));
 }
 
 export function showTaskCreation(e) {
-    body.append(view.get('taskCreationTemplate'));
-    taskCreate(e);
-    const closeButton = document.querySelector('.taskCreationLayout .crossIco');
-    closeButton.addEventListener('click', () => {
-        document.querySelector('.taskCreationLayout').remove();
-    })
+  body.append(view.get('taskCreationTemplate'));
+  taskCreate(e);
+  const closeButton = document.querySelector('.taskCreationLayout .crossIco');
+  closeButton.addEventListener('click', () => {
+    document.querySelector('.taskCreationLayout').remove();
+  });
 }
 
 function hideTaskCreation() {
-    body.removeChild(document.querySelector('.taskCreationLayout'));
+  body.removeChild(document.querySelector('.taskCreationLayout'));
 }
 
 export function showFilter() {
-    if (document.querySelector('.filterLayout')) hideFilter();
-    else {
-        filterView.display();
-    }
+  if (document.querySelector('.filterLayout')) hideFilter();
+  else {
+    filterView.display();
+  }
 }
 
 export function hideFilter() {
-    if (document.querySelector('.filterLayout')) {
-        body.removeChild(document.querySelector('.filterLayout'));
-    }
+  if (document.querySelector('.filterLayout')) {
+    body.removeChild(document.querySelector('.filterLayout'));
+  }
 }
 
 export function showError() {
-    body.append(view.get('errorModal'));
+  body.append(view.get('errorModal'));
 }
-
 
 /////////////////////////////////////////////////////////
--- scripts/controller/registration.js
+++ scripts/controller/registration.js
@@ -1,34 +1,56 @@
-import { crutchLogin } from "../../script.js";
-import { userDB } from "../../index.js";
-import { popUp } from "../helpers/popUp.js";
+import { crutchLogin } from '../../script.js';
+import { userDB } from '../../index.js';
+import { popUp } from '../helpers/popUp.js';
 
 export function registration() {
-    const regForm = document.querySelector('form[name="registration"]');
-    const regFormButton = regForm.querySelector('button')
+  const regForm = document.querySelector('form[name="registration"]');
+  const regFormButton = regForm.querySelector('button');
 
-    regFormButton.addEventListener('click', () => {
-        let regData = {
-            name: regForm[0].value,
-            login: regForm[1].value,
-            password: regForm[2].value,
-            repeatPassword: regForm[3].value,
-        };
-    
-        const existCheck = !!userDB.userArray.find((element) => {
-            return regData.login === element.login;
-        })
-        if (regData.login === '' || regData.password === '' || regData.repeatPassword === '' || regData.name === '') return alert();
-        if (existCheck) {
-            popUp('Email already used');
-        } else {
-            if ((regData.password === regData.repeatPassword) && (regData.password.length >= 6)) {
-                if (userDB.create(regData.login, regData.password, regData.name)) {
-                    userDB.saveCurrentUserInLocalStorage(userDB.userArray.at(-1));
-                    userDB.saveUserArrayInLocalStorage();
-                    crutchLogin();
-                } else popUp(`User with this email already exist.`);
+  regFormButton.addEventListener('click', () => {
+    let regData = {
+      name: regForm[0].value,
+      login: regForm[1].value,
+      password: regForm[2].value,
+      repeatPassword: regForm[3].value,
+    };
 
-            } else popUp(`Wrong password`);
-        }
-    })
+    const existCheck = !!userDB.userArray.find((element) => {
+      return regData.login === element.login;
+    });
+    if (
+      regData.login === '' ||
+      regData.password === '' ||
+      regData.repeatPassword === '' ||
+      regData.name === ''
+    ) {
+      return popUp('Fill in the fields');
+    }
+
+    if (existCheck) {
+      popUp('Email already used');
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(regData.login)
+    ) {
+      popUp('The email was entered incorrectly');
+      return;
+    } else {
+      if (
+        regData.password === regData.repeatPassword &&
+        regData.password.length >= 6
+      ) {
+        if (userDB.create(regData.login, regData.password, regData.name)) {
+          userDB.saveCurrentUserInLocalStorage(userDB.userArray.at(-1));
+          userDB.saveUserArrayInLocalStorage();
+          crutchLogin();
+        } else popUp(`User with this email already exist.`);
+      }
+      if (regData.password !== regData.repeatPassword) {
+        popUp(
+          `The password and the confirmation of the password do not match. Try again`
+        );
+      } else {
+        popUp(`The password must be at least 6 characters long`);
+      }
+    }
+  });
 }
